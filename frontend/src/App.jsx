35c71abd0b825669a8be98698806391ba957175f
edmar.james <edmarjamesbautista@gmail.com>
2024-11-14 15:23:07 +0800

    Create frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState }          from 'react';
+import reactLogo             from './assets/react.svg';
+import viteLogo              from '/vite.svg';
+import './App.css';
+
+// components
+import Cards from './components/Cards';
+
+
+function App() {
+
+  return (
+    <>
+      <Cards/>
+    </>
+  )
+};
+
+export default App
---
+++ frontend/src/components/Cards.jsx
@@ -0,0 +1,18 @@
+import React                 from 'react';
+import PropTypes             from 'prop-types';
+
+
+export default function Cards() {
+  return (
+    <div>
+        <p>id</p>
+        <p>question</p>
+        <p>answer</p>
+    </div>
+  )
+};
+
+// Cards.propTypes = {
+
+// }
+
